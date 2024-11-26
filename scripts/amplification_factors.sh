1ffa97a85ae1cf9ba8d3e1ff7fa8f4868646e789
Dahee Kwon <daheekwon@kaist.ac.kr>
2024-11-26 14:20:46 +0900

    add diffusers

--- scripts/amplification_factors.sh
+++ scripts/amplification_factors.sh
@@ -6,7 +6,7 @@ thres=80
 model="sdxl-turbo" #For SDXL Lightning 1-step, use "sdxl-light-1"
 
 ### 1) Sampling Imges
-python sampling.py --obj "$obj" --n_samples 100 --model "$model"
+python src/sampling.py --obj "$obj" --n_samples 100 --model "$model"
 
 ### 2) Computing the Usability Score
 start_seed=0
@@ -14,10 +14,10 @@ end_seed=9
 for ((seed=$start_seed; seed<=$end_seed; seed++))
 do
     seed_value=$((seed * 10))
-    python sampling_scores.py --start_seed $seed_value --obj "$obj" --model "$model"
+    python src/sampling_scores.py --start_seed $seed_value --obj "$obj" --model "$model"
 done 
 
 ### 3) Extracting Optimal Amplification Factors with usability
-python sampling_final_scores.py --obj "$obj" --thres "$thres" --model "$model"
+python src/sampling_final_scores.py --obj "$obj" --thres "$thres" --model "$model"
 
 
